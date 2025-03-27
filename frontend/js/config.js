ee344f0a14ef4b5cb8c99e9982ba42fcd62b0956
winksplorer <winksplorer@gordae.com>
2025-03-27 10:22:47 -0700

    start work on settings

---
+++ frontend/js/config.js
@@ -0,0 +1,21 @@
+function compactui(enabled) {
+    document.getElementById('nav-right').style.flexDirection = enabled ? "row" : "column";
+    document.getElementById('hostname').style.fontSize = enabled ? "1.5em" : "2em";
+}
+function segmentstats(enabled) {
+    document.getElementById('stats').style.fontFamily = enabled ? '"Seven Segment", sans-serif' : '"Helvetica Neue Condensed", sans-serif';
+}
+
+document.getElementById('compactui').addEventListener('change', function() {
+    const data = {
+        checked: this.checked
+    };
+    compactui(data.checked)
+});
+
+document.getElementById('segmentstats').addEventListener('change', function() {
+    const data = {
+        checked: this.checked
+    };
+    segmentstats(data.checked)
+});
