4eadfea92547f9b29b357c50d08fce26b80bfee2
winksplorer <wink@winksplorer.net>
2025-08-08 01:39:27 -0700

    fix sidebar width again, also fix a long hostname being cut off weirdly (its still weird but at least it doesn't go off the edge)

--- frontend/js/main/ui.js
+++ frontend/js/main/ui.js
@@ -42,7 +42,7 @@ _.ui.darkMode = (enabled) => {
 }
 
 // changes sidebar width
-_.ui.sidebarWidth = (width) => $('sidebar').style.minWidth = `${width}px`;
+_.ui.sidebarWidth = (width) => $('sidebar').style.minWidth = $('sidebar').style.maxWidth = `${width}px`;
 
 _.onReady(() => {
     // inital navbar fill
