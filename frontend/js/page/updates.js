7e34f0ac680c4f4579d35dfb91aa77818ce5405b
prescott2m <p2m@prescott2m.com>
2026-01-04 18:36:05 -0800

    frontend: updates poc

---
+++ frontend/js/page/updates.js
@@ -0,0 +1,36 @@
+// summit frontend/js/page/updates.js - handles the updates page
+
+_.onReady(() => {
+    // container title = package manager version
+     _.comm.request('updates.pkgmgr').then(ver => {
+        $('updates_header').textContent = ver
+    });
+
+    // update indexes
+    _.comm.request('updates.updateindex').then(() => {
+        $('updates_updating-index').style.display = 'none';
+
+        _.comm.request('updates.list').then(pkgs => {
+            const frag = document.createDocumentFragment();
+            pkgs.sort((a, b) => a.name.localeCompare(b.name));
+
+            frag.append(
+                _.helpers.newButton('upgrade all', !pkgs.length, null),
+                _.helpers.newEl('hr', '', '')
+            );
+
+            for (const pkg of pkgs) {
+                const inputPair = _.helpers.newEl('div', 'input-pair', '');
+                inputPair.append(
+                    _.helpers.newEl('span', '', `${pkg.name}: ${pkg.current_ver} -> ${pkg.new_ver}`),
+                    _.helpers.newButton('upgrade', false, null)
+                );
+
+                frag.appendChild(inputPair);
+                pkgs[pkgs.length-1].name !== pkg.name && frag.appendChild(_.helpers.newEl('hr', '', ''));
+            }
+
+            $('updates_pkgs').appendChild(frag);
+        });
+    }).catch(e => _.ui.dispatchMsg("couldn't update package index", e));
+})
